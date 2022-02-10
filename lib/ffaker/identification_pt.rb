eb21803197bed05cc6cf49ba11def8facf95e07e
German Michelena <german.michelena@diagroup.com>
2022-02-10 18:53:38 +0100

    Fix PT NIF algorithm

--- lib/ffaker/identification_pt.rb
+++ lib/ffaker/identification_pt.rb
@@ -7,21 +7,18 @@ module FFaker
 
 
     def nif
-      numero = rand(10_000_000...99_999_999)
-      letra = "TRWAGMYFPDXBNJZSQVHLCKE"[numero % 23].chr
-      numero + letra
 
       generated_nif = (1 + rand(9)).to_s
 
       (1..7).to_a.each { generated_nif += (rand(10)).to_s }
 
-      control_sum  = 9*value[0,1].to_i + 8*value[1,1].to_i + 7*value[2,1].to_i + 6*value[3,1].to_i + 5*value[4,1].to_i + 4*value[5,1].to_i + 3*value[6,1].to_i + 2*value[7,1].to_i
+      control_sum  = 9*generated_nif[0,1].to_i + 8*generated_nif[1,1].to_i + 7*generated_nif[2,1].to_i + 6*generated_nif[3,1].to_i + 5*generated_nif[4,1].to_i + 4*generated_nif[5,1].to_i + 3*generated_nif[6,1].to_i + 2*generated_nif[7,1].to_i
       control_mod  = control_sum % 11
       check_number = 11 - control_mod
-      check_number > 9 ? 0 : check_number
+      check_number =  check_number > 9 ? 0 : check_number
 
 
-      generated_nif += check_number
+      generated_nif += check_number.to_s
     end
   end
 end
